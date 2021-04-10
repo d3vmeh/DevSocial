0993a798d1473ef70c28e303480eb2f525c35d35
d3vmeh <63991511+d3vmeh@users.noreply.github.com>
2021-04-10 16:07:51 -0700

    Added friends features

--- app.py
+++ app.py
@@ -32,13 +32,19 @@ def landingPage():
     #     password = i["password"]
     #     date = i["date"]
     #     friends = [i["email"]]
-    #     i = {"firstname":firstname,"lastname":lastname,"email":email,"password":password,"date":date,"friends":friends}
+    #     requests = []
+    #     i = {"firstname":firstname,"lastname":lastname,"email":email,"password":password,"date":date,"friends":friends,"requests":requests}
         
-    #     mongo.db.users.delete_one({"email":"sumeet.mehra@gmail.com"})
+    #     mongo.db.users.delete_one({"email":email})
     #     mongo.db.users.insert_one(i)
-    #     break
-    
-    return render_template("index.html")
+    #     print(i)
+        
+    if "email" in session:
+        user = mongo.db.users.find_one({"email":session["email"]})
+        return render_template("index.html",requestcount=len(user["requests"]))
+
+    return render_template("index.html",requestcount=0)
+
 
 @app.route("/home")
 def homepage():
@@ -51,13 +57,14 @@ def homepage():
         for i in mongo.db.posts.find():
             #print(i)
             i["color"] = random.choice(colors)
-            allposts.append(i)
+            if i["email"] in user["friends"]:
+                allposts.append(i)
             
         
         allposts.reverse()
 
         #print("ehhlo",allposts[0]["email"])
-        return render_template("home.html",username=user["firstname"],posts=allposts)#,postcontent=allposts[0]["post"],posttime=allposts[0]["time"])
+        return render_template("home.html",username=user["firstname"],posts=allposts,requestcount=len(user["requests"]))#,postcontent=allposts[0]["post"],posttime=allposts[0]["time"])
 
 
     else:
@@ -66,7 +73,7 @@ def homepage():
 @app.route("/register",methods=["GET","POST"])
 def register():
     if (request.method == "GET"):
-        return render_template("register.html")
+        return render_template("register.html",requestcount=0)
     else:
         
         firstname = request.form["firstname"]
@@ -79,9 +86,9 @@ def register():
             flash("Email already in use","error")
             return redirect("/register")
         
-        mongo.db.users.insert_one({"firstname":firstname,"lastname":lastname,"email":email,"password":password,"date":date,"friends":[email]})
+        mongo.db.users.insert_one({"firstname":firstname,"lastname":lastname,"email":email,"password":password,"date":date,"friends":[email],"requests":[]})
         
-        return redirect("/login")
+        return redirect("/logout")
 
 
 @app.route("/login",methods=["GET","POST"])
@@ -91,7 +98,10 @@ def login():
         return redirect("/home")
     else:
         if (request.method == "GET"):
-            return render_template("login.html")
+            if "email" in session:
+                user = mongo.db.users.find_one({"email":session["email"]})
+                return render_template("login.html",requestcount=len(user["requests"]))
+            return render_template("login.html",requestcount=0)
         else:
             givenemail = request.form["email"]
             useraccount = mongo.db.users.find_one({"email":givenemail})
@@ -127,7 +137,8 @@ def createpost():
         return redirect("/login")
     else:
         if (request.method == "GET"):
-            return render_template("createpost.html")
+            user = mongo.db.users.find_one({"email":session["email"]})
+            return render_template("createpost.html",requestcount=len(user["requests"]))
         else:
             givenpost = request.form["post"]
             #print(len(givenpost))
@@ -138,7 +149,97 @@ def createpost():
     return redirect("/login")
 
 
+@app.route("/addfriend",methods=["GET","POST"])
+def addfriend():
+    if "email" not in session:
+        #print("not logged in")
+        return redirect("/login")
+    if (request.method == "GET"):
+        user = mongo.db.users.find_one({"email":session["email"]})
+        return render_template("addfriend.html",requestcount=len(user["requests"]))
+    else:
+        user = mongo.db.users.find_one({"email":session["email"]})
+        requestemail = request.form["email"]
+        friendrequested = mongo.db.users.find_one({"email":requestemail})
+        #print(friendrequested)
+        otherrequests=friendrequested["requests"]
 
+        if requestemail in user["friends"]:
+            flash("You are already friends with this user!")
+            return redirect("/addfriend")
+        
+        if user["email"] in friendrequested["requests"]:
+            flash("You already sent a friend request to this user!")
+            return redirect("/addfriend")
+
+        otherrequests.append(session["email"])
+        
+        mongo.db.users.update_one({"email":requestemail},{"$set":{"requests":otherrequests}})
+        return redirect("/home")
+        
+
+@app.route("/requests",methods=["GET","POST"])
+def requests():
+    if "email" not in session:
+        #print("not logged in")
+        return redirect("/login")
+    if (request.method == "GET"):
+        user = mongo.db.users.find_one({"email":session["email"]})
+
+        return render_template("requests.html",requests=user["requests"],requestcount=len(user["requests"]))
+    else:
+        user = mongo.db.users.find_one({"email":session["email"]})
+        myrequests = user["requests"]
+        myfriends = user["friends"]
+        # requestemail = request.form["email"]
+        # friendrequested = mongo.db.users.find_one({"email":requestemail})
+        # #print(friendrequested)
+        # otherrequests=friendrequested["requests"]
+        # otherrequests.append(session["email"])
+        
+        otheruseremail = request.form["r"]
+        otheruser = mongo.db.users.find_one({"email":otheruseremail})
+        otherfriends = otheruser["friends"]
+        #mongo.db.users.update_one({"email":requestemail},{"$set":{"requests":otherrequests}})
+        if (request.form["action"] == "Accept"):
+            myfriends.append(otheruseremail)
+            myrequests.remove(otheruseremail)
+            otherfriends.append(user["email"])
+            mongo.db.users.update_one({"email":user["email"]},{"$set":{"requests":myrequests,"friends":myfriends}})
+            mongo.db.users.update_one({"email":otheruser["email"]},{"$set":{"friends":otherfriends}})
+            
+            return redirect("/requests")
+        if (request.form["action"] == "Decline"):
+            myrequests.remove(otheruseremail)
+            mongo.db.users.update_one({"email":user["email"]},{"$set":{"requests":myrequests}})
+            return redirect("/requests")
+
+        return redirect("/home")
+
+
+@app.route("/friends",methods=["GET","POST"])
+def friends():
+    if "email" not in session:
+        #print("not logged in")
+        return redirect("/login")
+    if (request.method == "GET"):
+        user = mongo.db.users.find_one({"email":session["email"]})
+        f = user["friends"]
+        f.remove(user["email"])
+        return render_template("friends.html",requests=user["requests"],requestcount=len(user["requests"]),friends=f)
+    else:
+        user = mongo.db.users.find_one({"email":session["email"]})
+        friendemail = request.form["friendemail"]
+        friend = mongo.db.users.find_one({"email":friendemail})
+        myfriends = user["friends"]
+        myfriends.remove(friendemail)
+        friendsfriends = friend["friends"]
+        friendsfriends.remove(user["email"])
+
+        mongo.db.users.update_one({"email":user["email"]},{"$set":{"friends":myfriends}})
+        mongo.db.users.update_one({"email":friendemail},{"$set":{"friends":friendsfriends}})
+
+        return redirect("/friends")
 
 
 if __name__ == "__main__":
